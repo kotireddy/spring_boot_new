30ca793e3a63f7422ac7b84b94387ac45afb3387
kotireddy <kotireddyare@gmail.com>
2018-06-05 14:36:33 +0530

    Hibernate SessionFactory & Transaction manager Bean Creation done....

--- src/main/java/com/spring/boot/config/CustomApplicationConfiguration.java
+++ src/main/java/com/spring/boot/config/CustomApplicationConfiguration.java
@@ -1,11 +1,8 @@
 package com.spring.boot.config;
 
-import java.util.Arrays;
-
-import javax.naming.NamingException;
-import javax.sql.DataSource;
-
+import com.zaxxer.hikari.HikariDataSource;
 import org.apache.catalina.connector.Connector;
+import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -16,43 +13,65 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
 import org.springframework.core.env.Environment;
 import org.springframework.jndi.JndiObjectFactoryBean;
-
-import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.Arrays;
+import java.util.Properties;
 
 @Configuration
+@EnableTransactionManagement
 public class CustomApplicationConfiguration {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomApplicationConfiguration.class);
 	
 	@Autowired
 	private Environment environment;
-	
+
 	@Bean
-	public DataSource dataSource() throws Exception {
+	public DataSource dataSource() {
 		LOGGER.info("<<<<<<< Jndi DataSource Fetching >>>>>>>");
-		if(Arrays.asList(environment.getActiveProfiles()).contains("prod")) {
-			LOGGER.info("<<<<<<< Jndi Prod DataSource Fetching >>>>>>>");
-			JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
-			bean.setJndiName("java:comp/env/jdbc/oxygen");
-			bean.setProxyInterface(DataSource.class);
-			bean.setLookupOnStartup(false);
-			bean.afterPropertiesSet();
-			return (DataSource) bean.getObject();
-		}else {
-			LOGGER.info("<<<<<<< UAT (OR) DEV DataSource Fetching >>>>>>>");
-			DataSourceProperties dataSourceProperties = dataSourceProperties();
-			return primaryDataSource(dataSourceProperties);
+		try{
+			if(Arrays.asList(environment.getActiveProfiles()).contains("prod")) {
+				LOGGER.info("<<<<<<< Jndi Prod DataSource Fetching >>>>>>>");
+				JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+				bean.setJndiName("java:comp/env/jdbc/oxygen");
+				bean.setProxyInterface(DataSource.class);
+				bean.setLookupOnStartup(false);
+				bean.afterPropertiesSet();
+				return (DataSource) bean.getObject();
+			}else {
+				LOGGER.info("<<<<<<< UAT (OR) DEV DataSource Fetching >>>>>>>");
+				DataSourceProperties dataSourceProperties = dataSourceProperties();
+				return primaryDataSource(dataSourceProperties);
+			}
+		}catch(Exception e){
+			return null;
 		}
+
 	}
 
 	@Bean
-	public LocalSessionFactoryBean sessionFactory() throws  Exception{
+	public SessionFactory sessionFactory() {
 		LocalSessionFactoryBean sessionFactory = new LocalSessionFactoryBean();
 		sessionFactory.setDataSource(dataSource());
-		return sessionFactory;
+		sessionFactory.setPackagesToScan("com.spring.boot.entity");
+		Properties hibernateProperties = new Properties();
+		hibernateProperties.put("hibernate.current_session_context_class",
+				"org.springframework.orm.hibernate5.SpringSessionContext");
+		sessionFactory.setHibernateProperties(hibernateProperties);
+		return sessionFactory.getObject();
 	}
-	
+
+	@Bean
+	public HibernateTransactionManager transactionManager(SessionFactory sessionFactory) {
+		HibernateTransactionManager transactionManager = new HibernateTransactionManager();
+		transactionManager.setSessionFactory(sessionFactory);
+		return transactionManager;
+	}
+
 	@Bean
 	@Primary
 	@ConfigurationProperties("spring.datasource")
