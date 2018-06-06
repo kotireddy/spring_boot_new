b375de163b882af9eb390092fe5d4bd8a3ef9b7b
kotireddy <kotireddyare@gmail.com>
2018-06-06 11:12:53 +0530

    After Solving issues on JPA Transaction manager & SessionFactory

--- src/main/java/com/spring/boot/config/CustomApplicationConfiguration.java
+++ src/main/java/com/spring/boot/config/CustomApplicationConfiguration.java
@@ -7,18 +7,20 @@ import org.hibernate.SessionFactory;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceProperties;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.orm.hibernate5.HibernateTransactionManager;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.context.annotation.Primary;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.sql.DataSource;
+import javax.persistence.EntityManagerFactory;
 
 @Configuration
+@EnableTransactionManagement
 public class CustomApplicationConfiguration {
 
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomApplicationConfiguration.class);
@@ -28,13 +30,41 @@ public class CustomApplicationConfiguration {
 
 	@Bean
 	@ConfigurationProperties(prefix = ApplicationContants.DATABASE_PROPS_CONST)
-	public HikariDataSource dataSource(DataSourceProperties dataSourceProperties) {
+	public HikariDataSource dataSource() {
 		LOGGER.info("<<< --- Loading HikariDataSource --- >>>");
-		return dataSourceProperties.initializeDataSourceBuilder()
+		return propertyLoaderConfiguration.dataSourceProperties().initializeDataSourceBuilder()
 									.type(HikariDataSource.class).build();
 	}
 
+	@Bean
+	@Primary
+	public EntityManagerFactory entityManagerFactoryBean(){
+		HibernateJpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+		vendorAdapter.setGenerateDdl(true);
+		LocalContainerEntityManagerFactoryBean entityManagerFactory = new
+										LocalContainerEntityManagerFactoryBean();
+		entityManagerFactory.setJpaVendorAdapter(vendorAdapter);
+		entityManagerFactory.setDataSource(dataSource());
+		entityManagerFactory.setPackagesToScan("com.spring.boot.entity");
+		entityManagerFactory.setPersistenceUnitName("default");
+		entityManagerFactory.afterPropertiesSet();
+		return entityManagerFactory.getObject();
+	}
 
+    @Bean
+	public SessionFactory sessionFactory(EntityManagerFactory entityManagerFactory) {
+		if (entityManagerFactory.unwrap(SessionFactory.class) == null) {
+			throw new NullPointerException("factory is not a hibernate factory");
+		}
+		return entityManagerFactory.unwrap(SessionFactory.class);
+    }
+
+    @Bean
+	public PlatformTransactionManager transactionManager(){
+		JpaTransactionManager txManager = new JpaTransactionManager();
+		txManager.setEntityManagerFactory(entityManagerFactoryBean());
+		return txManager;
+	}
 
 	public static Connector getTomcatConnector(){
 		Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
