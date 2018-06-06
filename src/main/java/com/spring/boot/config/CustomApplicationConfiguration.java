80403148ba5f2e37f3936c038ec25a94a8b2d160
kotireddy <kotireddyare@gmail.com>
2018-06-06 12:16:59 +0530

    After adding UAT,PROD,DEV Profiles configuration....

--- src/main/java/com/spring/boot/config/CustomApplicationConfiguration.java
+++ src/main/java/com/spring/boot/config/CustomApplicationConfiguration.java
@@ -11,6 +11,8 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Primary;
+import org.springframework.core.env.Environment;
+import org.springframework.jndi.JndiObjectFactoryBean;
 import org.springframework.orm.jpa.JpaTransactionManager;
 import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
 import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
@@ -18,6 +20,8 @@ import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
 import javax.persistence.EntityManagerFactory;
+import javax.sql.DataSource;
+import java.util.Arrays;
 
 @Configuration
 @EnableTransactionManagement
@@ -26,14 +30,33 @@ public class CustomApplicationConfiguration {
 	private static final Logger LOGGER = LoggerFactory.getLogger(CustomApplicationConfiguration.class);
 
 	@Autowired
+	private Environment environment;
+
+	@Autowired
 	private PropertyLoaderConfiguration propertyLoaderConfiguration;
 
 	@Bean
 	@ConfigurationProperties(prefix = ApplicationContants.DATABASE_PROPS_CONST)
-	public HikariDataSource dataSource() {
+	public DataSource dataSource() {
 		LOGGER.info("<<< --- Loading HikariDataSource --- >>>");
-		return propertyLoaderConfiguration.dataSourceProperties().initializeDataSourceBuilder()
-									.type(HikariDataSource.class).build();
+		try{
+			if(Arrays.asList(environment.getActiveProfiles()).contains("prod")) {
+				LOGGER.info("<<<<<<< Jndi Prod DataSource Fetching >>>>>>>");
+				JndiObjectFactoryBean bean = new JndiObjectFactoryBean();
+				bean.setJndiName("java:comp/env/jdbc/oxygen");
+				bean.setProxyInterface(DataSource.class);
+				bean.setLookupOnStartup(false);
+				bean.afterPropertiesSet();
+				return (DataSource)bean.getObject();
+			}else{
+				LOGGER.info("<<<<<<< UAT (OR) DEV DataSource Fetching >>>>>>>");
+				return propertyLoaderConfiguration.dataSourceProperties()
+						.initializeDataSourceBuilder().type(HikariDataSource.class).build();
+			}
+		}catch(Exception e){
+			return null;
+		}
+
 	}
 
 	@Bean
